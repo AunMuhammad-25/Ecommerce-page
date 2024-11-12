6131491ccdb30fcef864fc57be2b6d44a823d129
qamberali <qamberali4904@gmail.com>
2024-11-12 16:10:05 +0500

    E commerce

--- signup.js
+++ signup.js
@@ -4,7 +4,7 @@ $(function() {
      e.preventDefault();
  
      var fname = $("#username").val();
-     var lname = $("email").val();
+     var lname = $("#email").val();
      var mname = $("#password").val();
      var nname = $("#confirmpassword").val();
  
@@ -21,15 +21,17 @@ $(function() {
          $("#span2").text("")
      }
      if(mname == ""){
-        $("#span1").text("This field is required")
+        $("#span3").text("This field is required")
     }
     else{
-        $("#span1").text("")
-    }if(nname == ""){
-        $("#span2").text("This field is required")
+        $("#span3").text("")
+    }
+     
+    if(nname == ""){
+        $("#span4").text("This field is required")
     }
     else{
-        $("#span2").text("")
+        $("#span4").text("")
     }
      if(fname !== "" && lname !== "" && mname !== "" && nname !== "" ){
          window.location.href = "thankyou.html"
